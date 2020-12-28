ec0de0c4d18bcdb04db025faad18f23a53dbf2b9
timokie <timokie@126.com>
2020-12-28 23:23:25 +0800

    feat: 洋葱圈实现

--- JKoa/index.js
+++ JKoa/index.js
@@ -5,11 +5,16 @@ const request = require('./request')
 const response = require("./response")
 
 class JKoa {
+  constructor() {
+    this.middlewares = []
+  }
   listen(...args) {
-    const server = http.createServer((req, res)=> {
+    const server = http.createServer(async(req, res)=> {
       // 创建上下文
       let ctx = this.createContext(req, res)
-      this.callback(ctx)
+      // 中间件合成
+      const fn = this.compose(this.middlewares);
+      await fn(ctx)
       // 响应
       res.end(ctx.body)
     })
@@ -17,7 +22,8 @@ class JKoa {
     server.listen(...args)
   }
   use(callback) {
-    this.callback = callback
+    // 将中间件加到数组里
+    this.middlewares.push(callback)
   }
   createContext(req, res) {
     const ctx = Object.create(context)
@@ -28,6 +34,21 @@ class JKoa {
     ctx.res = ctx.response.res = res
 
     return ctx
+  }
+  // 合成函数
+  compose(middlewares) {
+    return function(ctx) {
+      return dispatch(0)
+      function dispatch(i) {
+        let fn = middlewares[i]
+        if(!fn) {
+          return Promise.resolve()
+        }
+        return Promise.resolve(fn(ctx, function next(){
+          return dispatch(i+1)
+        }))
+      }
+    }
   }
 }
 module.exports= JKoa
