83fcddd1dbaf5f0f01e74f53156a4e5b17b79cd7
timokie <timokie@126.com>
2020-12-28 23:26:58 +0800

    feat: test洋葱圈模型

--- index.js
+++ index.js
@@ -24,10 +24,26 @@
 const JKoa = require('./JKoa')
 const app = new JKoa()
 
-app.use((ctx) => {
-  ctx.body = 'hello, timokie'
+// app.use((ctx) => {
+//   ctx.body = 'hello, timokie'
+// });
+const delay = () => new Promise(resolve => setTimeout(() => resolve(), 2000));
+app.use(async (ctx, next) => {
+  ctx.body = "1";
+  await next();
+  ctx.body += "5";
+});
+app.use(async (ctx, next) => {
+  ctx.body += "2";
+  await delay();
+  await next();
+  ctx.body += "4";
+});
+app.use(async (ctx, next) => {
+  ctx.body += "3";
 });
 
+// 以上输出“12345”
 app.listen(8081, () => {
   console.log("app is starting at http://localhost:8081");
 })
